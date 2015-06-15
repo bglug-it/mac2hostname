f8b67cddb65f4d84f8218aa12da2a666db4f9b24
enricobacis <enrico.bacis@gmail.com>
2015-06-15 23:22:16 +0200

    Add route /whatsmyhostname that automatically gets the connection mac address.

--- mac2hostname.py
+++ mac2hostname.py
@@ -3,9 +3,11 @@
 """API to map MAC addresses to hostnames"""
 
 from contextlib import contextmanager, closing
+from subprocess import check_output
 from sqlite3 import connect
 from bottle import route, run, request
 from json import dumps
+import re
 
 __author__ = "Enrico Bacis"
 __email__ = "enrico.bacis@gmail.com"
@@ -22,13 +24,6 @@ def init_tables():
                        'hostname TEXT NOT NULL UNIQUE, mac TEXT UNIQUE)')
         cursor.execute('CREATE INDEX IF NOT EXISTS idxmac ON client(mac)')
 
-@route('/hosts')
-def hosts():
-    with getcursor() as cursor:
-        return dumps([dict((meta[0], data)
-                      for meta, data in zip(cursor.description, row))
-                      for row in cursor.execute('SELECT hostname, mac FROM client ORDER BY id')], indent=4)
-
 def normalizemac(mac):
     return ':'.join(x.zfill(2) for x in mac.split(':')).upper()
 
@@ -40,12 +35,29 @@ def gethostname(mac, base=None):
         (hostname,) = cursor.execute('SELECT hostname FROM client WHERE mac = "%s"' % mac)
     return hostname
 
+def getmac(ip):
+    check_output(['ping', '-c1', '-t2', ip])
+    arp = check_output(['arp', '-n', ip])
+    return re.search(r'(([\da-fA-F]{1,2}\:){5}[\da-fA-F]{1,2})', arp).group(1)
+
+@route('/hosts')
+def hosts():
+    with getcursor() as cursor:
+        return dumps([dict((meta[0], data)
+                      for meta, data in zip(cursor.description, row))
+                      for row in cursor.execute('SELECT hostname, mac FROM client ORDER BY id')], indent=4)
+
 @route('/mac2hostname')
 def mac2hostname():
     mac, base = request.query.mac, request.query.base
     if not mac:
         return 'Usage: GET /mac2hostname?mac=XX_XX_XX_XX_XX_XX[&base=YYY]'
     return gethostname(mac, base)
+
+@route('/whatsmyhostname')
+def whatsmyhostname():
+    ip = request.query.ip or request['REMOTE_ADDR']
+    return gethostname(getmac(ip), request.query.base)
 
 if __name__ == '__main__':
     init_tables()
