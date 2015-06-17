07525e705c59b8dbeaa58e77a1a953d445794a1b
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2015-06-17 15:05:27 +0200

    Sources tarball generation script added.

---
+++ build_src.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name=mac2hostname
+version=1.0.0
+tmpdir=/tmp/src_build/${name}-${version}
+
+cwd=$(pwd)
+mkdir -p ${tmpdir}
+cp mac2hostname mac2hostname.py mac2hostname.ini README.md LICENSE ${tmpdir}
+cd ${tmpdir}/..
+tar czvf ${cwd}/${name}-${version}.tar.gz ${name}-${version}
+
+# Cleanup
+rm -rf /tmp/src_build
